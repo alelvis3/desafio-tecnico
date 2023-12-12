c479baa0688bf30a44d1c2c8c291cb9a7345d543
alelvis3 <alelvis3br@hotmail.com>
2023-12-12 17:45:53 -0300

    Atualização do codigo e Readme
    Signed-off-by: alelvis3 <alelvis3br@hotmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-A sample command-line application providing basic argument parsing with an entrypoint in `bin/`.
+# README
+
+Bem-vindo!
+
+Teste para Vaga de DESENVOLVEDOR BACKEND - INICIANTE - ESTAGIÁRIO - TRAINEE
+
+Projeto Dart usando IntelliJ
+## Pré-requisitos
+
+Certifique-se de ter os seguintes itens instalados em sua máquina antes de começar:
+
+- [Dart SDK](https://dart.dev/get-dart)
+- [IntelliJ IDEA](https://www.jetbrains.com/idea/download/)
+
+## Configuração do Ambiente
+
+1. **Instale o Dart SDK:**
+   Certifique-se de que o Dart SDK está instalado em sua máquina. Você pode seguir as instruções de instalação [aqui](https://dart.dev/get-dart).
+
+2. **Instale o IntelliJ IDEA:**
+   Faça o download e instale o IntelliJ IDEA a partir do [site oficial](https://www.jetbrains.com/idea/download/).
+
+3. **Instale o plugin Dart no IntelliJ:**
+   Abra o IntelliJ IDEA, vá para "Preferences" (ou "Settings" no Windows), clique em "Plugins", procure por "Dart" e instale o plugin.
+
+4. **Configuração do SDK no IntelliJ:**
+   Vá para "Preferences" (ou "Settings" no Windows), clique em "Languages & Frameworks" > "Dart". Certifique-se de que o SDK do Dart está configurado corretamente.
+
+## Rodando o Projeto
+
+1. **Clone o repositório:**
+   ```bash
+   git clone https://github.com/alelvis3/desafio-tecnico.git
+   cd seu-projeto-dart
+
+Execute o programa dentro da IDE 
+
+
+# Obrigado pela oportunidade!! :-)
--- bin/desafio_tecnico.dart
+++ bin/desafio_tecnico.dart
@@ -1,14 +1,41 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  int numero = 10;
-  int soma = 0;
-  if (numero > 0) {
-    for (int i = 3; i < numero; i++) {
-      if ((i % 3) == 0 || (i % 5) == 0) {
-        soma = soma + i;
+  funcMenu();
+}
+
+void funcMenu() {
+  print(
+      "***************************************************************************************************");
+  print("Desafio Técnico 1\n"
+      "Implemente uma função que receba um número inteiro positivo e retorne o somatório de todos os valores\n"
+      "inteiros divisíveis por 3 ou 5 que sejam inferiores ao número passado.");
+  print(
+      "***************************************************************************************************");
+
+    print("Digite o numero intero para receber a soma");
+
+    try {
+      String? input = stdin.readLineSync();
+      int numero = int.parse(input!);
+      if (numero > 0) {
+        int soma = funcDesafio(numero);
+        print("A soma dos valore é $soma");
+      } else {
+        print("O numero não pode ser negativo!!");
       }
+    } catch (e) {
+      print("Digite somete numeros inteiros positivo");
     }
-    print('Somatoria dos numeros é $soma');
-  } else {
-    print("O numero não pode ser negativo");
   }
+
+
+int funcDesafio(int numero) {
+  int soma = 0;
+  for (int i = 3; i < numero; i++) {
+    if ((i % 3) == 0 || (i % 5) == 0) {
+      soma = soma + i;
+    }
+  }
+  return soma;
 }
